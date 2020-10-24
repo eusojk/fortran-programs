1b814afaa4c5fcafb46889cfd125a32f9558faa8
eusojk <josue.kpodo@gmail.com>
2020-10-24 01:18:33 -0400

    prime numbers

--- exo/exo4_5.f90
+++ exo/exo4_5.f90
@@ -1,7 +1,39 @@
 program exo4_5
 
-    !  A program which readsan integer value limit and print the first limit prime numbers, by any method.
+    !  A program which reads an integer value limit and print the first limit prime numbers, by any method.
     implicit none
-    integer :: limit
+    integer :: limit, limit_counter = 0, number = 2
+
+    write(6, '(a)', advance='no') "Number of primes: "
+    read(5, '(i3)') limit
+    write(6, '(a, i3, a)') 'The first ', limit, ' prime number(s) are: '
+
+    do 
+        if ( is_prime(number) .eqv. .true. ) then
+            limit_counter = limit_counter + 1
+            write(6, '(i5, a)', advance='no') number, ' '
+        end if
+
+        if ( limit_counter .eq. limit ) then
+            write(6, '(a)') ' '
+            exit
+        end if
+
+        number = number + 1
+    end do
+
+contains
+
+    logical function is_prime(n)
+        integer, intent(in) :: n
+        integer             :: iter_i
+        is_prime  = .true.
+        do iter_i = 2, int( sqrt( real(n) ) ) 
+            if ( modulo(n, iter_i) .eq. 0 ) then
+                is_prime  = .false.
+                exit
+            end if
+        end do
+    end function is_prime
 
 end program exo4_5
